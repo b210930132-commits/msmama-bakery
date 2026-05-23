e17e5202ee4f6f153a0ea2a44cc3bc06ac6efafb
EMUUJIN <b210930132@erdenetis.edu.mn>
2026-05-23 21:02:05 +0800

    fix secure session

--- config/session.php
+++ config/session.php
@@ -198,9 +198,9 @@ return [
     | Supported: "lax", "strict", "none", null
     |
     */
+'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
-    'same_site' => env('SESSION_SAME_SITE', 'lax'),
-
+'secure' => env('SESSION_SECURE_COOKIE', false),
     /*
     |--------------------------------------------------------------------------
     | Partitioned Cookies
